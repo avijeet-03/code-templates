de41556fb1cc9b79536b2fcefb17fd61ee2f4188
Avijeet Nayak <avijeetnayak001@gmail.com>
2024-09-07 04:44:31 +0530

    update sparse table code

--- SparseTable.cpp
+++ SparseTable.cpp
@@ -1,46 +1,78 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
 
 struct SparseTable {
-	const int LOG = 25;
-	vector<int> bin_log;
-	vector<vector<int>> val;
-	int n;
 
-	SparseTable (int len) {
-		n = len;
-		bin_log = vector<int> (n + 1);
-		val = vector<vector<int>> (n + 1, vector<int> (LOG));
-		// val[i][j] :- result of the range [i.....i + 2^j - 1]
-	}
+    const int LOG = 25;
+    vector<vector<int>> val;
+    int n;
 
-	int merge(int a, int b) {
-		// idempotent operation
-		// operation can be applied multiple times
-		// without changing the result
-		return min(a, b);
-	}
+    SparseTable(vector<int> &arr) {
+        n = arr.size();
+        val = vector<vector<int>> (n, vector<int> (LOG));
 
-	void build(vector<int> &a) {
-		bin_log[1] = 0;
-		for (int i = 2; i <= n; i++)
-			bin_log[i] = bin_log[i / 2] + 1;
+        // val[i][j] will store the combined information of the subarray
+        // starting from i and length 2^j
 
-		for (int i = 0; i < n; i++)
-			val[i][0] = a[i];
+        for (int i = 0; i < n; i++)
+            val[i][0] = arr[i];
 
-		for (int j = 1; j < LOG; j++) {
-			for (int i = 0; i + (1 << j) - 1 < n; i++) {
-				val[i][j] = merge(val[i][j - 1], val[i + (1 << (j - 1))][j - 1]);
-			}
-		}
-		// O(n * logn)
-	}
+        for (int j = 1; j < LOG; j++) {
+            for (int i = 0; i < n; i++) {
+                int nextInd = i + (1 << (j - 1));
+                if (nextInd >= n) break;
+                val[i][j] = merge(val[i][j - 1], val[nextInd][j - 1]);
+            }
+        }
+    }
 
-	int query(int l, int r) {
-		int length = r - l + 1;
-		int k = bin_log[length];
-		return merge(val[l][k], val[r - (1 << k) + 1][k]);
-	}
+    int merge(int a, int b) {
+        // idempotent operations
+        // with two values, do the required operations
+        // e.g. gcd, min, max
+        return min(a, b);
+    }
+
+    int getAnswer(int i, int j) {
+        // for subarray i to j, what is the answer
+        int length = j - i + 1;
+        int bin_log_length = log2(length);
+
+        // both subarray length should be bin_log_length
+        // one will start from index i
+        // another will end at index j
+
+        int left_part_answer = val[i][bin_log_length];
+        int right_part_answer = val[j + 1 - (1 << bin_log_length)][bin_log_length];
+
+        return merge(left_part_answer, right_part_answer);
+    }
 };
 
+void test_case() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    SparseTable s(a);
+
+    while (q--) {
+        int i, j;
+        cin >> i >> j;
+        i--;
+        j--;
+        cout << s.getAnswer(i, j) << "\n";
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    test_case();
+    return 0;
+}
+
 // Problem Link: https://cses.fi/problemset/task/1647/
-// Submission Link: https://cses.fi/paste/a532be5ca0fdb542382baa/
